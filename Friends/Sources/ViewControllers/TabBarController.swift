dfaaecaf14b82318c10745e7036d9e4a346d9d1c
Gorazd Kunej <gorazd@neutrino8.com>
2021-04-01 15:24:08 +0200

    Project init.

---
+++ Friends/Sources/ViewControllers/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  Friends
+//
+//  Created by Gorazd Kunej on 01/04/2021.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.isHidden = true
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
