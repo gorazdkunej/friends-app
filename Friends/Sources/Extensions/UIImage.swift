9cee99ed7bf8228e2a72f5b895f7f2fc9a864ed2
Gorazd Kunej <gorazd@neutrino8.com>
2021-04-03 16:27:05 +0200

    Implemented UIColor extension for masking image with color.

--- Friends/Sources/Extensions/UIImage.swift
+++ Friends/Sources/Extensions/UIImage.swift
@@ -34,4 +34,22 @@ extension UIImage {
         UIGraphicsEndImageContext()
         return result
     }
+    
+    func maskWithColor(color: UIColor) -> UIImage {
+
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        let context = UIGraphicsGetCurrentContext()!
+
+        let rect = CGRect(origin: CGPoint.zero, size: size)
+
+        color.setFill()
+        self.draw(in: rect)
+
+        context.setBlendMode(.sourceIn)
+        context.fill(rect)
+
+        let resultImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return resultImage
+    }
 }
