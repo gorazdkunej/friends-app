7c1ced0b97d02bd771a3bcb088bdc38b13ac9630
Gorazd Kunej <gorazd@neutrino8.com>
2021-04-03 16:26:16 +0200

    Added custom colors to assets file.

---
+++ Friends/Sources/Extensions/UIColor.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor.swift
+//  Friends
+//
+//  Created by Gorazd Kunej on 03/04/2021.
+//
+
+import Foundation
---
+++ Friends/Sources/ViewControllers/UI/SkratchSegmentControl.swift
@@ -0,0 +1,8 @@
+//
+//  SkratchSegmentControl.swift
+//  Friends
+//
+//  Created by Gorazd Kunej on 03/04/2021.
+//
+
+import Foundation
