8a751678f7062a94acb9f2b10770c88112595a8c
Gorazd Kunej <gorazd@neutrino8.com>
2021-04-03 16:26:37 +0200

    Implemented UIColor extension.

--- Friends/Sources/Extensions/UIColor.swift
+++ Friends/Sources/Extensions/UIColor.swift
@@ -5,4 +5,44 @@
 //  Created by Gorazd Kunej on 03/04/2021.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    public enum skratch { }
+}
+
+extension UIColor.skratch {
+    static var navy: UIColor {
+        guard let color = UIColor(named: "navy") else {
+            return .black
+        }
+        return color
+    }
+    static var navy50: UIColor {
+        guard let color = UIColor(named: "Navy50%") else {
+            return .darkGray
+        }
+        return color
+    }
+    
+    static var paleBlue: UIColor {
+        guard let color = UIColor(named: "PaleBlue") else {
+            return .blue
+        }
+        return color
+    }
+    
+    static var purple: UIColor {
+        guard let color = UIColor(named: "Purple") else {
+            return .purple
+        }
+        return color
+    }
+    
+    static var text: UIColor {
+        guard let navy = UIColor(named: "Text") else {
+            return .darkGray
+        }
+        return navy
+    }
+}
